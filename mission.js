3770253db3d5cff78f5a76e53e715c92712fd1a4
Neta Cohen <neta2232@walla.co.il>
2025-03-20 21:13:29 +0200

    add the files

--- mission.js
+++ mission.js
@@ -1,34 +1,38 @@
-function handleNewClick(e, color) {    
-    e.target.style.color = color;    
+function addFeatures(){
+let namevalue = String(document.querySelector('#nameofpro').value)
+let pricevalue = String(document.querySelector('#priceofpro').value)
+let optionselected = String(document.querySelector('#food').value)
+let Image1 =document.querySelector('#linkvalue').value;
+let x = document.querySelector('#thecart');
+let x1 = document.createElement('tr');
+let x2 = document.createElement('td');
+let x3 = document.createElement('td');
+let x4 = document.createElement('td');
+let x5 = document.createElement('td');
+let newimg = `<img width="200" src = ${Image1}>`
+x2.innerText = "Product Name: "+ namevalue;
+x3.innerText = "Product Price: "+ pricevalue;
+x4.innerText = "catagory: "+ optionselected;
+x5.innerHTML = newimg;
+
+
+
+
+x.appendChild(x1);
+x1.appendChild(x2);
+x1.appendChild(x3);
+x1.appendChild(x4);
+x1.appendChild(x5);
+
+
+
+
 }
-
-const newElem = document.createElement("button");
-newElem.innerText = "yo"
-// newElem.onclick = (e) => { handleNewClick(e, 'red') };
-newElem.addEventListener("click", (e) => { handleNewClick(e, 'red') })
-
-document.body.appendChild(newElem)
+function clearform(){
+    document.querySelector('#nameofpro').value = "";
+    document.querySelector('#priceofpro').value = "";
+    document.querySelector('#linkvalue').value = "";
+    document.querySelector('#food').value = "";
 
 
-
-
-
-
-
-
-// newElem.onclick = (moshe) => { handleNewClick(moshe, color) };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// newElem.addEventListener("click", func)
-// newElem.addEventListener("click", handleNewClick2)
+}
